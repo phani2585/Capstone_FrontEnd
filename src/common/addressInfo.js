40ca16b9060dc1211b81d9b72e2266987db2c779
phani2585 <>
2019-07-04 20:34:11 +0530

    Tab component- Stepper component in Checkout 90% done

---
+++ src/common/addressInfo.js
@@ -0,0 +1,25 @@
+let addressInfo = [
+    {
+      "id": "7b3fe6a4-6add-428f-8ba4-7428497270eb",
+      "flat_building_name": "a-509",
+      "locality": "BKC",
+      "city": "bandra",
+      "pincode": "400020",
+      "state": {
+        "id": "c860e78a-a29b-11e8-9a3a-720006ceb890",
+        "state_name": "Maharashtra"
+      }
+    },
+    {
+      "id": "cf38c624-4145-4a17-a15b-c4dc49fdeb73",
+      "flat_building_name": "c-509",
+      "locality": "Gopal Nager",
+      "city": "worli",
+      "pincode": "400018",
+      "state": {
+        "id": "c860e78a-a29b-11e8-9a3a-720006ceb890",
+        "state_name": "Maharashtra"
+      }
+    }
+  ]
+  export default addressInfo;
--- src/screens/checkout/Checkout.js
+++ src/screens/checkout/Checkout.js
@@ -1,8 +1,77 @@
 import React, { Component } from 'react';
 import './Checkout.css';
+import * as Utils from "../../common/Utils";
+import * as UtilsUI from "../../common/UtilsUI";
+import * as Constants from "../../common/Constants";
 import Header from '../../common/header/Header';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-//import Button from '@material-ui/core/Button';
+import { withStyles } from "@material-ui/core/styles";
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepContent from '@material-ui/core/StepContent';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import addressInfo from '../../common/addressInfo';
+
+// inline styles for Material-UI components
+const styles = theme => ({
+
+    root: {
+      width: '90%',
+    },
+    rootTab: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+    },
+    imagesGridList: {
+        margin: "15px !important"
+      },
+    button: {
+      marginTop: theme.spacing(1),
+      marginRight: theme.spacing(1),
+    },
+    actionsContainer: {
+      marginBottom: theme.spacing(2),
+    },
+    resetContainer: {
+      padding: theme.spacing(3),
+    },
+    paymentroot: {
+      display: 'flex',
+    },
+    paymentformControl: {
+      margin: theme.spacing(3),
+    },
+    paymentgroup: {
+      margin: theme.spacing(1, 0),
+    },
+  });
+
+/**
+ * Functional component for displaying Tab components
+ * @param props properties passed by parent component to child component
+ */
+const TabContainer = function (props) {
+    return (
+        <Typography component="div" style={{ padding: 0, textAlign: 'center' }}>
+            {props.children}
+        </Typography>
+    )
+}
+
+TabContainer.propTypes = {
+    children: PropTypes.node.isRequired
+}
+  
+  
 
 
 class Checkout extends Component {
@@ -11,14 +80,47 @@ class Checkout extends Component {
         this.baseUrl = "http://localhost:8080/api";
     }
     
+    state = {
+        activeStepValue : 0,
+        steps : ['Delivery','Payment'],
+        tabValue :0
+        
+    };
     
+    handleBackHandler = () => { this.setState({ activeStepValue : this.state.activeStepValue - 1 });}
+
+    handleNextHandler = () => { this.setState({ activeStepValue : this.state.activeStepValue + 1 });}
+    
+    handleResetHandler = () => { this.setState({ activeStepValue : 0 });}
+
+    /**
+    * Event handler called when the user tries to navigate to different tabs
+    * @memberof Checkout
+    */
+   tabChangeHandler = (event, value) => {this.setState({ value });}
+
+
+    
+      
+  /*  stepContentHandler = () => {
+       if(this.state.activeStepValue === 0) {
+        this.setState({ IsDeliveryContentOpen  : true}) ;
+       }
+       if(this.state.activeStepValue === 1){
+        this.setState({ IsPaymentContentOpen  : true}) ;
+       }
+    }*/
+       
+            
+         
+     
 
    /**
    * Function called when the component is rendered
    * @memberof Details
    */
   render() {
-   // const { classes } = this.props;
+   const { classes } = this.props;
 
     return (
       <MuiThemeProvider>
@@ -31,11 +133,88 @@ class Checkout extends Component {
             showProfile={true}
             enableMyAccount={false}
           />
-          <div className="checkout-body-container">
-              This is Checkout page
+          
+          <div className={classes.root}>
+      <Stepper activeStep={this.state.activeStepValue} orientation="vertical">
+        {this.state.steps.map((label) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+            <StepContent>
+        <Typography>
+        {this.state.activeStepValue === this.state.steps.length - 1 ?
+            (<div>Payment Notes</div>)
+            :(<div className={classes.rootTab}>
+                <AppBar position="static">
+                  <Tabs value={this.state.tabValue} onChange={this.tabChangeHandler}>
+                    <Tab label="EXISTING ADDRESS" />
+                    <Tab label="NEW ADDRESS" />
+                  </Tabs>
+                </AppBar>
+                {this.state.tabValue === 0 &&
+                     <TabContainer>
+                         {!Utils.isUndefinedOrNull(addressInfo) ? (
+                <GridList
+                  cellHeight={350}
+                  cols={3}
+                  className={classes.imagesGridList}
+                >
+                  {addressInfo.map((address, index) => (
+                    <GridListTile
+                      className="address-tile"
+                      key={"grid" + address.id}
+                    >
+                     <div className="address-format">
+                     {address.flat_building_name},<br/>
+                     {address.locality},<br/>
+                     {address.city}<br/>
+                     {address.state.state_name}<br/>
+                     {address.pincode}<br/>
+                     </div> 
+                    </GridListTile>
+                  ))}
+                </GridList>
+              ) : null}
+              </TabContainer>}
+                {this.state.tabValue === 1 && <TabContainer>NEW ADDRESS</TabContainer>}
+                
+              </div>)
+            }
+            </Typography>
+              <div className={classes.actionsContainer}>
+                <div>
+                  <Button
+                    disabled={this.state.activeStepValue === 0}
+                    onClick={this.handleBackHandler}
+                    className={classes.button}
+                  >
+                    Back
+                  </Button>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={this.handleNextHandler}
+                    className={classes.button}
+                  >
+                    {this.state.activeStepValue === this.state.steps.length - 1 ? 'Finish' : 'Next'}
+                  </Button>
+                </div>
+              </div>
+            </StepContent>
+          </Step>
+        ))}
+      </Stepper>
+      {this.state.activeStepValue === this.state.steps.length && (
+        <Paper square elevation={0} className={classes.resetContainer}>
+          <Typography>View the summary &amp; place your order now! </Typography>
+          <Button onClick={this.handleResetHandler} className={classes.button}>
+           Change
+          </Button>
+        </Paper>
+      )}
+    </div>
               
           </div>
-          </div>
+        
           </MuiThemeProvider>
                 
        
@@ -43,4 +222,4 @@ class Checkout extends Component {
     }
 }
 
-export default Checkout;
+export default withStyles(styles) (Checkout);
